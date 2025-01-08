f009d201e22bde5c74926f3c3f06e464bcc084f2
Kriti Dixit <kritidixit1@gmail.com>
2025-01-08 10:26:41 +0530

    API commit

--- src/test/java/Assignement_API/Assignement_2.java
+++ src/test/java/Assignement_API/Assignement_2.java
@@ -1,2 +1,25 @@
-package Assignement_API;public class Assignement_2 {
+package Assignement_API;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class Assignement_2 {
+    @Test
+    public void testCoinDesk() {
+        RestAssured.baseURI = "https://api.coindesk.com/";
+        String response = given().log().all()
+                .when().get("/v1/bpi/currentprice.json")
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .body("bpi.USD", notNullValue())
+                .body("bpi.GBP", notNullValue())
+                .body("bpi.EUR", notNullValue())
+                .body("bpi.GBP.description", equalTo("British Pound Sterling"))
+                .extract().response().asString();
+
+        System.out.println(response);
+    }
 }
